59d0a6b3daa20ca1b09ae5d325e4c59b98ee6895
Heize <dev.heize@gmail.com>
2024-03-11 14:58:12 +0900

    add3

--- calculator.py
+++ calculator.py
@@ -1,8 +1,7 @@
-#heize_더하기
+#이지현_더하기
 
 def addition():
     num1 = float(input("첫 번째 숫자를 입력하세요: "))
     num2 = float(input("두 번째 숫자를 입력하세요: "))
     result = num1 + num2
     print("결과: ", result)
-
