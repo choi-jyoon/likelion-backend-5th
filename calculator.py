66630f35f2c9d17989f2946cc8391ff51d4f1600
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2024-03-11 15:14:21 +0900

    merge: merga all

--- calculator.py
+++ calculator.py
@@ -1,15 +1,5 @@
 a, b = map(int, input("두 숫자를 입력하세요: ").split())
 
-def divide(a, b):
-    return a / b
-
-def sub(a, b):
-
-    print(sub(a, b))
-    return a - b
-
-def multiply(a,b):
-    return a*b
 #이지현_더하기
 
 def addition():
@@ -17,3 +7,14 @@ def addition():
     num2 = float(input("두 번째 숫자를 입력하세요: "))
     result = num1 + num2
     print("결과: ", result)
+    
+def sub(a, b):
+
+    print(sub(a, b))
+    return a - b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a, b):
+    return a / b
