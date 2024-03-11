2823e42c3343e511f6406e467266575ae3dd3df1
Heize <dev.heize@gmail.com>
2024-03-11 14:53:55 +0900

    add2

--- heize-add.py
+++ heize-add.py
@@ -1,14 +1,7 @@
-#heize
+#이지현_더하기
 
-def add(num1, num2):
-    return num1 + num2
-
-def main():
-    print("더하기 계산기입니다.")
+def addition():
     num1 = float(input("첫 번째 숫자를 입력하세요: "))
     num2 = float(input("두 번째 숫자를 입력하세요: "))
-    result = add(num1, num2)
-    print("결과:", result)
-
-if __name__ == "__main__":
-    main()
+    result = num1 + num2
+    print("결과: ", result)
